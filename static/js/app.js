dec60f91a17a75c7aba8b2368bbaba344cc9988b
sajeanpage <sarjepage@gmail.com>
2022-10-14 23:11:52 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-    const url = '/data/samples.json';
+    const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
     var test_subjects;    
     var demographics; 
